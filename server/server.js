7255e4306ce77bf2cda616018230c1ee351f6061
luweijia <luweijia1013@sjtu.edu.cn>
2019-05-23 18:23:39 +0100

    server with some error handle

--- server/server.js
+++ server/server.js
@@ -1,26 +1,43 @@
-// Minimal server: log request details
+// Server which delivers only static HTML pages (no content negotiation).
+// Response codes: see http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+// When the global data has been initialised, start the server.
 let HTTP = require('http');
+let FS = require('fs').promises;
+let OK = 200, NotFound = 404, BadType = 415;
 start(8080);
 
 // Provide a service to localhost only.
 function start(port) {
-  let service = HTTP.createServer(handle);
-  service.listen(port, 'localhost');
-  console.log("Visit localhost:" + port);
+    let service = HTTP.createServer(handle);
+    try { service.listen(port, 'localhost'); }
+    catch (err) { throw err; }
+    console.log("Visit localhost:" + port);
 }
 
 // Deal with a request.
-function handle(request, response) {
-  console.log("Method:", request.method);
-  console.log("URL:", request.url);
-  console.log("Headers:", request.headers);
-  reply(response);
+async function handle(request, response) {
+    let url = request.url;
+    if (url.endsWith("/")) url = url + "index.html";
+    if (! url.endsWith(".html")) return fail(response, BadType, "Not .html");
+    let file = "" + url;
+    let content;
+    try { content = await FS.readFile(file); }
+    catch (err) { return fail(response, NotFound, file + " Not found"); }
+    reply(response, content);
 }
 
 // Send a reply.
-function reply(response) {
-  let hdrs = { 'Content-Type': 'text/plain' };
-  response.writeHead(200, hdrs);  // 200 = OK
-  response.write("OK");
-  response.end();
+function reply(response, content) {
+    let hdrs = { 'Content-Type': 'application/xhtml+xml' };
+    response.writeHead(OK, hdrs);
+    response.write(content);
+    response.end();
+}
+
+// Send a failure message
+function fail(response, code, message) {
+    let hdrs = { 'Content-Type': 'text/plain' };
+    response.writeHead(code, hdrs);
+    response.write(message);
+    response.end();
 }
