342a2fedd33adedc621b3df8742d4496909ce8e7
JinyangZhan <wsweisu@gmailcom>
2019-11-22 17:50:07 +0000

    fix xpath

--- GP_Spider/GP_Spider/spiders/google.py
+++ GP_Spider/GP_Spider/spiders/google.py
@@ -8,41 +8,42 @@ from scrapy import Request
 class GoogleSpider(scrapy.Spider):
     name = 'google'
     allowed_domains = ['google.play.com']
-    start_urls = ['https://play.google.com/store']
+    start_urls = []
 
+    def start_requests(self):
+        url = 'https://play.google.com/store'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + '/search?q=' + tag + '&c=apps'
+            print(url)
+        yield scrapy.Request(url, self.parse)
+
+    '''
     def parse(self, response):
         keywords = [
             'stuttering', 'speech%20therapy', 'speech%20and%20language%20therapy', 'aphasia', 'apraxia', 'dysarthria'
         ]
 
-        link_flag = 0
-
-        urls = []
-        for each in keywords:
-            app_url = ("https://play.google.com/store/search?q=" + keywords[link_flag] + '&c=apps')
+        for key in keywords:
+            app_url = ("https://play.google.com/store/search?q=" + key + "&c=apps")
             print(app_url)
             yield Request(url=app_url, callback=self.parse_search, dont_filter=True)
-            link_flag += 1
+    '''
 
-    def parse_search(self, response):
+    def parse(self, response):
         print("START PARSING")
         selector = scrapy.Selector(response)
         #print(response.body)
 
-        urls = selector.xpath('//div[@class="Qktxw"]/div[@class="WpDbMd"]/div[@class="T4LgNb"]/div[@class="N4FjMb"]/div[@class="WcZbQd"]/div[@class="Ktdaqe"]/div[@class="ZmHEEd"]/div[@class="ImZGtf mpg5gc"]/div[@class="uMConb  V2Vq5e POHYmb-eyJpod YEDFMc-eyJpod y1APZe-eyJpod drrice"]/a[@class="poRVub" and aria-hidden="true"]/@href').extract()
+        #urls = selector.xpath('//a[@class="poRVub" and aria-hidden="true"]/@href').extract()
         #urls = selector.xpath('//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/c-wiz/c-wiz/c-wiz/div/div[2]/div[1]/c-wiz/div/div/div/div/div/a/@href').extract()
+        urls = selector.xpath('//a[@class="JC71ub"]/@href').extract()
 
         print(urls)
 
-        link_flag = 0
-        links = []
-        for link in urls:
-            links.append(link)
-
-        for each in urls:
-            yield Request(url="https://play.google.com" + links[link_flag], callback=self.parse_detail, dont_filter=True)
-            print("https://play.google.com" + links[link_flag])
-            link_flag += 1
+        for app_url in urls:
+            yield Request(url="https://play.google.com" + app_url, callback=self.parse_detail, dont_filter=True)
+            print("https://play.google.com" + app_url)
 
     def parse_detail(self, response):
         item = GpItem()
